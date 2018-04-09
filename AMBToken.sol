170ee02dfaaf25c25103c64d3c4126d0db4e66ea
davit.kochiashvili@bitbucket.com <atlassianvici1>
2018-04-09 20:24:34 +0400

    final contract

--- AMBToken.sol
+++ AMBToken.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.21;
 
-import './math/SafeMath.sol';
+import "./SafeMath.sol";
 
 contract AMBToken {
     using SafeMath for uint256;
@@ -48,7 +48,7 @@ contract AMBToken {
 
     function _approve(address _spender, uint256 _value) internal returns (bool) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -61,43 +61,45 @@ contract AMBToken {
         require(_to != address(0));
         require(_value <= investors[_from].tokenBalance);
 
-        fixDividendBalances(_to,false);
+        fixDividendBalances(_to, false);
 
         investors[_from].tokenBalance = investors[_from].tokenBalance.sub(_value);
         investors[_to].tokenBalance = investors[_to].tokenBalance.add(_value);
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(investors[msg.sender].activated && contractIsWorking);
-        fixDividendBalances(msg.sender,false);
+        fixDividendBalances(msg.sender, false);
         return _transfer( msg.sender, _to,  _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(investors[msg.sender].activated && investors[_from].activated && contractIsWorking);
-
         require(_to != address(0));
         require(_value <= investors[_from].tokenBalance);
         require(_value <= allowed[_from][msg.sender]);
 
-        fixDividendBalances(_from,false);
-        fixDividendBalances(_to,false);
+        fixDividendBalances(_from, false);
+        fixDividendBalances(_to, false);
 
         investors[_from].tokenBalance = investors[_from].tokenBalance.sub(_value);
-        investors[_to].tokenBalance = investors[_to].tokenBalance.add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
+        investors[_to].tokenBalance   = investors[_to].tokenBalance.add(_value);
+        allowed[_from][msg.sender]    = allowed[_from][msg.sender].sub(_value);
+
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
     /*
         Eligible token and balance helper function
      */
-    function fixDividendBalances(address investor, bool revertIfClaimed) internal returns (InvestorProfitData storage current_profit,uint256 profit_per_token){
+    function fixDividendBalances(address investor, bool revertIfClaimed) internal
+        returns (InvestorProfitData storage current_profit, uint256 profit_per_token){
+
         uint32 next_id      = uint32(dividends.length);
-        uint32 current_id   = next_id-1;
+        uint32 current_id   = next_id - 1;
         current_profit      = profits[investor][current_id];
 
         if (revertIfClaimed) require(current_profit.status != ProfitStatus.Claimed);
@@ -108,14 +110,13 @@ contract AMBToken {
             current_profit.start_balance = investors[investor].tokenBalance;
             current_profit.end_balance   = investors[investor].tokenBalance;
             current_profit.status        = ProfitStatus.EndFixed;
-
             next_profit.start_balance = investors[investor].tokenBalance;
             next_profit.status        = ProfitStatus.StartFixed;
 
         } else if (current_profit.status == ProfitStatus.StartFixed) {
+
             current_profit.end_balance = investors[investor].tokenBalance;
             current_profit.status      = ProfitStatus.EndFixed;
-
             next_profit.start_balance = investors[investor].tokenBalance;
             next_profit.status        = ProfitStatus.StartFixed;
         }
